09626f5c353b3a3912c16499fb76fea6d6d2582f
chaudhuree <chaudhuree00@gmail.com>
2024-05-13 16:53:23 +0600

    update the notification controller, added postman collection file

--- controllers/notificationController.js
+++ controllers/notificationController.js
@@ -21,53 +21,106 @@ const createNotification = async (req, res) => {
   }
 };
 
-// get notifications by user
-const getNotificationsByUser = async (req, res) => {
-  try {
-    const notifications = await Notification.find({ user: req.user }).sort({ createdAt: -1 });
-    const totalNotifications = await Notification.countDocuments({ user: req.user });
-    res.status(StatusCodes.OK).json({ success: true, totalNotifications, notifications });
-  } catch (error) {
-    res
-      .status(StatusCodes.INTERNAL_SERVER_ERROR)
-      .json({ success: false, message: error.message });
-  }
-}
-  
 // mark notification as read
 const markNotificationAsRead = async (req, res) => {
   try {
     const { id: notificationId } = req.params;
-    const notification = await Notification.findOneAndUpdate( { _id: notificationId }, { read: true }, { new: true });  
+    const notification = await Notification.findOneAndUpdate(
+      { _id: notificationId },
+      { read: true },
+      { new: true }
+    );
     if (!notification) {
       return res
         .status(StatusCodes.NOT_FOUND)
         .json({ success: false, message: "Notification not found" });
     }
-    res.status(StatusCodes.OK).json({ success: true, message: "notification marked as read", notification });
-  }
-  catch (error) {
     res
-      .status(StatusCodes.INTERNAL_SERVER_ERROR)
-      .json({ success: false, message: error.message });
-  }
-}
-
-//  mark all notifications as read
-const markAllNotificationsAsRead = async (req, res) => {
-  try {
-    const notifications = await Notification.updateMany({ user: req.user }, { read: true });
-    res.status(StatusCodes.OK).json({ success: true, message: "All notifications marked as read", notifications });
+      .status(StatusCodes.OK)
+      .json({
+        success: true,
+        message: "notification marked as read",
+        notification,
+      });
   } catch (error) {
     res
       .status(StatusCodes.INTERNAL_SERVER_ERROR)
       .json({ success: false, message: error.message });
   }
-}
+};
+
+//  mark all notifications as read
+const markAllNotificationsAsRead = async (req, res) => {
+  try {
+    const notifications = await Notification.updateMany(
+      { user: req.user },
+      { read: true }
+    );
+    res
+      .status(StatusCodes.OK)
+      .json({
+        success: true,
+        message: "All notifications marked as read",
+        notifications,
+      });
+  } catch (error) {
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ success: false, message: error.message });
+  }
+};
+
+// get notification of user by read status
+const getNotificationsByReadStatus = async (req, res) => {
+  try {
+    const { read } = req.query;
+    if (read === "true") {
+      const notifications = await Notification.find({
+        user: req.user,
+        read: true,
+      });
+      const totalNotifications = await Notification.countDocuments({
+        user: req.user,
+        read: true,
+      });
+      res
+        .status(StatusCodes.OK)
+        .json({ success: true, totalNotifications, notifications });
+    } else if (read === "false") {
+      const notifications = await Notification.find({
+        user: req.user,
+        read: false,
+      });
+      const totalNotifications = await Notification.countDocuments({
+        user: req.user,
+        read: false,
+      });
+      res
+        .status(StatusCodes.OK)
+        .json({ success: true, totalNotifications, notifications });
+    } else if (read === "all") {
+      const notifications = await Notification.find({ user: req.user });
+      const totalNotifications = await Notification.countDocuments({
+        user: req.user,
+      });
+      res
+        .status(StatusCodes.OK)
+        .json({ success: true, totalNotifications, notifications });
+    } else {
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ success: false, message: "Invalid query parameter" });
+    }
+  } catch (error) {
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ success: false, message: error.message });
+  }
+};
 
 module.exports = {
   createNotification,
-  getNotificationsByUser,
+  getNotificationsByReadStatus,
   markNotificationAsRead,
   markAllNotificationsAsRead,
 };
--- routes/notification.js
+++ routes/notification.js
@@ -3,13 +3,14 @@ const express = require("express");
 const router = express.Router();
 
 const { createNotification,
-  getNotificationsByUser,
+  getNotificationsByReadStatus,
   markNotificationAsRead,
   markAllNotificationsAsRead} = require('../controllers/notificationController.js');
 const { requireAuth } = require('../middleware/authentication.js');
 
 router.route("/notification").post(requireAuth, createNotification);
-router.route("/notifications").get(requireAuth, getNotificationsByUser);
+// {{baseUrl}}/api/v1/notifications?read=true
+router.route("/notifications").get(requireAuth, getNotificationsByReadStatus);
 router.route("/notification/:id").put(requireAuth, markNotificationAsRead);
 router.route("/notifications/markallasread").put(requireAuth, markAllNotificationsAsRead);
 
